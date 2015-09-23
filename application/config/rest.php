3a03387250a7c3eac0f7f12e0df3e94816907bfd
Minhaj Alam <rubaiat.csedu.10@gmail.com>
2015-09-24 02:10:15 +0600

    enable logging for rest calls

--- application/config/rest.php
+++ application/config/rest.php
@@ -359,7 +359,7 @@ $config['rest_key_name'] = 'X-API-KEY';
 |   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
 */
-$config['rest_enable_logging'] = FALSE;
+$config['rest_enable_logging'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -370,7 +370,7 @@ $config['rest_enable_logging'] = FALSE;
 | table name to match e.g. my_logs
 |
 */
-$config['rest_logs_table'] = 'logs';
+$config['rest_logs_table'] = 'opds_logs';
 
 /*
 |--------------------------------------------------------------------------
--- tools/dbexport.sh
+++ tools/dbexport.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mysqldump -u root -p123qwe  opencart opds_authors opds_books opds_book_to_author opds_catagory opds_catalogs opds_publishers opds_users opds_storages opds_files opds_user_to_file > data/db.sql
+mysqldump -u root -p123qwe  opencart opds_authors opds_books opds_book_to_author opds_catagory opds_catalogs opds_publishers opds_users opds_storages opds_files opds_user_to_file opds_product_to_book_files opds_logs > data/db.sql
